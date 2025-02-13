39fa24621aa9d532bb4e78796900825542e7ee6f
dopatendo <andreschristiansen@proton.me>
2025-02-13 16:30:52 +0100

    initial

--- R/korea.R
+++
@@ -1,6 +0,0 @@
-#' ICILS 2023 Chilean student and school data
-#'
-#' @name chile
-#' @docType data
-#' @keywords data
-NULL
---
+++ R/oman.R
@@ -0,0 +1,6 @@
+#' ICILS 2023 Oman student and school data
+#'
+#' @name oman
+#' @docType data
+#' @keywords data
+NULL
