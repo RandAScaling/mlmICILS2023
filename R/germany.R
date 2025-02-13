6d219e2b0fd82d17647ee4c0e511827445f4edca
dopatendo <andreschristiansen@proton.me>
2025-02-13 16:16:23 +0100

    initial

---
+++ R/germany.R
@@ -0,0 +1,6 @@
+#' ICILS 2023 German student and school data
+#'
+#' @name germany
+#' @docType data
+#' @keywords data
+NULL
---
+++ R/korea.R
@@ -0,0 +1,6 @@
+#' ICILS 2023 Chilean student and school data
+#'
+#' @name chile
+#' @docType data
+#' @keywords data
+NULL
